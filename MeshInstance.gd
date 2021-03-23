f78ccad6e0767179071d9a92e0b73f89a03a2ce3
IsaacGuz19 <55608406+IsaacGuz19@users.noreply.github.com>
2021-03-22 20:27:08 -0700

    imprime choque si se tocan los objetos

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -1,8 +1,16 @@
-extends MeshInstance
+extends KinematicBody
 
 
 func _process(delta):
-	translation.z += 10 * delta
+	
 	if translation.z >= 0.715:
 		translation.z = -10.795
 		translation.x = rand_range(-5, 5)
+
+func _physics_process(delta):
+	
+	var choque = move_and_collide(Vector3(0,0,10 * delta))
+	if (choque is KinematicCollision):
+		print("Choque")
+	
+ 
--- Spatial.gd
+++ Spatial.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends KinematicBody
 
 
 
@@ -7,3 +7,18 @@ func _process(delta):
 		translation.x += 10 * delta
 	if Input.is_action_pressed("ui_left") and translation.x > -5:
 		translation.x -= 10 * delta
+
+#func is_colliding():
+	#var normal = get_collision_normal()
+	#movimiento = normal.slide(movimiento)
+	#velocidad = normal.slide(velocidad)
+	#move(movimiento)
+	#if(normal.y < 0):
+		#salto = false
+		
+#func procesar_colision():
+	#var obj_colision = get_collider()
+	#if(obj_colision.is_in_group("MeshInstance")):
+		#print("Choque")
+
+		
