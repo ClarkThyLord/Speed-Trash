4e3e1b2d966daa58c1e8c54e43875bf243c3527d
ClarkThyLord <ccpotential@gmail.com>
2021-04-04 16:07:11 -0700

    reworking the spawning of map objects
    Clark was here :V

--- src/map/map.gd
+++ src/map/map.gd
@@ -1,40 +1,55 @@
 extends Spatial
 
-export var max_obstacles := 5
-export var max_goals := 5
+
+
+## Exported Variables
+export(int, 0, 100) var map_objects_max := 5
+
+export(float, 0.0, 10.0) var map_objects_spawn := 1.5
+
+
 
 ## OnReady Variables
 onready var road_animation : AnimationPlayer = get_node("Road/AnimationPlayer")
 
-#onready var object : Spatial = get_node("Objects")
-#onready var obstacle : Spatial = get_node("Objects/Obstacle")
-#onready var goal : Spatial = get_node("Objects/Goal")
+onready var map_objects : Spatial = get_node("MapObjects")
 
-#onready var obstacle = preload("res://src/obstacles/obstacle.tscn")
-#onready var goal = preload("res://src/goals/goal.tscn")
 
-const obstacle = preload("res://src/obstacles/obstacle.tscn")
-const goal = preload("res://src/goals/goal.tscn")
 
-var objects = [
-	obstacle.instance(),
-	obstacle.instance(),
-	goal.instance(),
-	goal.instance(),
+## Private Variables
+var _spawn_time := 0.0
+
+var _spawn_points := [
+	Vector3(-5.5, 0, -50),
+	Vector3(-1.75, 0, -50),
+	Vector3(1.75, 0, -50),
+	Vector3(5.5, 0, -50),
 ]
+
+var _map_objects := [
+	preload("res://src/map/objects/trash/trash.tscn")
+]
+
+
 
 ## Built-In Virtual Methods
 func _ready() -> void:
-	randomize()
 	road_animation.play("moving")
-	_on_Timer_timeout()	
 	
-func spawn_objects():
 	randomize()
-	var i = randi() % objects.size()
-	var scene = objects[i]
-	add_child(scene)
 
 
-func _on_Timer_timeout():
-	spawn_objects()
+func _process(delta : float):
+	if map_objects.get_child_count() < map_objects_max and _spawn_time >= map_objects_spawn:
+		var map_object = _map_objects[randi() % _map_objects.size()].instance()
+		map_object.translation = _spawn_points[randi() % _spawn_points.size()]
+		map_objects.add_child(map_object)
+		
+		_spawn_time = 0.0
+	
+	for map_object in map_objects.get_children():
+		map_object.translation += Vector3(0, 0, 30 * map_object.speed * delta)
+		if map_object.translation.z > 0:
+			map_object.translation = _spawn_points[randi() % _spawn_points.size()]
+	
+	_spawn_time += delta 
