ace0ce9bd016cb9f3d5d5cefec0331223541cfff
ClarkThyLord <ccpotential@gmail.com>
2021-04-04 16:08:25 -0700

    clean up
    Clark was here :V

--- src/garbage_truck/garbage_truck.gd
+++ src/garbage_truck/garbage_truck.gd
@@ -10,9 +10,9 @@ export var speed := 10.0
 ## Built-In Virtual Methods
 func _process(delta):
 	var direction := Vector3.ZERO
-	if Input.is_action_pressed("ui_right") and translation.x < 3:
+	if Input.is_action_pressed("ui_right") and translation.x < 6:
 		direction += Vector3.RIGHT
-	if Input.is_action_pressed("ui_left") and translation.x > -3:
+	if Input.is_action_pressed("ui_left") and translation.x > -6:
 		direction += Vector3.LEFT
 	
 	move_and_collide(direction * speed * delta)
