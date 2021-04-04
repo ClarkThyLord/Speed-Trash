16eafea4865f58002938e3fe3a1f9ad2306fd3c0
ClarkThyLord <ccpotential@gmail.com>
2021-04-04 16:32:24 -0700

    added different type of trash
    Clark was here :V

--- src/map/objects/trash/trash.gd
+++ src/map/objects/trash/trash.gd
@@ -1,16 +1,54 @@
+tool
 extends "res://src/map/objects/map_object.gd"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+## Enums
+enum TrashTypes {
+	ORGANIC,
+	PAPER,
+	GLASS,
+	METAL,
+	PLASTIC,
+	E_WASTE,
+}
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+## Exported Variables
+export(TrashTypes) var trash_type := 0 setget set_trash_type
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+## OnReady Variables
+onready var texture : Sprite3D = get_node("Sprite3D")
+
+
+
+## Built-In Virtual Methods
+func _ready() -> void:
+	set_trash_type(trash_type)
+
+
+
+## Public Methods
+func set_trash_type(value : int) -> void:
+	var color : Color
+	match trash_type:
+		TrashTypes.ORGANIC:
+			color = Color(0.082353, 0.082353, 0.082353)
+		TrashTypes.PAPER:
+			color = Color(0, 0.215686, 1)
+		TrashTypes.GLASS:
+			color = Color.white
+		TrashTypes.METAL:
+			color = Color(1, 0.92549, 0)
+		TrashTypes.PLASTIC:
+			color = Color(1, 0.568627, 0)
+		TrashTypes.E_WASTE:
+			color = Color.red
+		_:
+			return
+	trash_type = value
+	
+	texture.modulate = color
