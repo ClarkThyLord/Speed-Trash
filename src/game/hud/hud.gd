0bcd75bd9f5d6f82b1af35fee606c4e4736a1e67
ClarkThyLord <ccpotential@gmail.com>
2021-04-17 21:52:38 -0700

    reposition score and correct aspect
    Clark was here :V

--- src/game/hud/hud.gd
+++ src/game/hud/hud.gd
@@ -9,4 +9,4 @@ onready var points := get_node("Points")
 
 ## Built-In Virtual Methods
 func _process(delta : float) -> void:
-	points.text = "POINTS: " + str(get_node("/root/Session").points)
+	points.text = str(get_node("/root/Session").points)
